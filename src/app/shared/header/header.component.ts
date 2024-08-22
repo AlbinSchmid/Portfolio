9508c81e07096ce3c4d781d438591b44e604a352
Albin Schmid <schmidalbin15@gmail.com>
2024-08-22 21:17:18 +0200

    added header and aot

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  germanLanguage: boolean = true;
+  englishLanguage: boolean = false;
 
+
+  changeLanguage(){
+    if (this.germanLanguage) {
+      this.englishLanguage = true;
+      this.germanLanguage = false;
+    } else if (this.englishLanguage) {
+      this.englishLanguage = false;
+      this.germanLanguage = true;
+    }
+  }
 }
