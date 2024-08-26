808d5282fb455a243ef1e6be39803534ce60b345
Albin Schmid <schmidalbin15@gmail.com>
2024-08-26 12:32:40 +0200

    finished desgin of skill set

--- src/app/main-page/skill-set/skill-set.component.ts
+++ src/app/main-page/skill-set/skill-set.component.ts
@@ -1,12 +1,65 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skill-set',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './skill-set.component.html',
   styleUrl: './skill-set.component.scss'
 })
 export class SkillSetComponent {
+  skillsIcons = [
+    {
+      'name': 'HTML',
+      'img': 'html',
+    },
+    {
+      'name': 'CSS',
+      'img': 'css',
+    },
+    {
+      'name': 'JavaScript',
+      'img': 'javascript',
+    },
+    {
+      'name': 'TypeScript',
+      'img': 'typescript',
+    },
+    {
+      'name': 'Angular',
+      'img': 'angular',
+    },
+    {
+      'name': 'Material Design',
+      'img': 'material-design',
+    },
+    {
+      'name': 'Firebase',
+      'img': 'firebase',
+    },
+    {
+      'name': 'GIT',
+      'img': 'git',
+    },
+    {
+      'name': 'Rest-Api',
+      'img': 'api',
+    },
+    {
+      'name': 'Scrum',
+      'img': 'scrum',
+    },
+  ];
+  hoveredOnGrowthImg = false;
 
+  showImg(){
+    if (this.hoveredOnGrowthImg) {
+      this.hoveredOnGrowthImg = false;
+    } else if (!this.hoveredOnGrowthImg) {
+      this.hoveredOnGrowthImg = true;
+    }
+  }
 }
